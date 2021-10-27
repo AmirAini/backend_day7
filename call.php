9540bc50c3c2fef575974f2c638c0520154fbb44
HiddenIsSquid <amirhaini@gmail.com>
2021-10-27 08:06:33 +0800

    add

--- call.php
+++ call.php
@@ -3,7 +3,10 @@
 
     // to store
     $kevin = new people('Kevin', '2000','You like to confuse yourself with OOP');
+    
+    //display
     echo $kevin -> dob();
+    
     // echo $kevin -> get_name();
     // echo $kevin -> get_dob();
     // echo $kevin -> get_hobby();
